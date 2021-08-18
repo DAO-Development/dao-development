a203f3f68a7aeb011aa2e6256067500b2874948e
life-f <58433392+life-f@users.noreply.github.com>
2021-08-18 13:48:20 +0300

    redisign reviews

--- #src/js/script.js
+++ #src/js/script.js
@@ -76,22 +76,30 @@ $(document).ready(function () {
     if ($(window).width() <= '568') {
         $('.reviews__slider').slick({
             variableWidth: false,
-            arrows: true,
-            prevArrow: '<div class="arrow-prev arrow"><img src="../img/arrow-prev.png"></div>',
-            nextArrow: '<div class="arrow-next arrow"><img src="../img/arrow-next.png"></div>',
-            dots: false,
+            dots: true,
+            arrows: false,
             infinite: false,
             slidesToShow: 1,
         });
     } else if ($(window).width() <= '768') {
         $('.reviews__slider').slick({
             variableWidth: false,
+            dots: true,
             arrows: true,
-            prevArrow: '<div class="arrow-prev arrow"><img src="../img/arrow-prev.png"></div>',
-            nextArrow: '<div class="arrow-next arrow"><img src="../img/arrow-next.png"></div>',
-            dots: false,
+            prevArrow: '<div class="arrow-prev arrow"></div>',
+            nextArrow: '<div class="arrow-next arrow"></div>',
             infinite: false,
             slidesToShow: 2,
+        });
+    }else if ($(window).width() > '768') {
+        $('.reviews__slider').slick({
+            variableWidth: false,
+            dots: true,
+            arrows: true,
+            prevArrow: '<div class="arrow-prev arrow"></div>',
+            nextArrow: '<div class="arrow-next arrow"></div>',
+            infinite: false,
+            slidesToShow: 3,
         });
     }
 
